117ec7b07ac3fa7cb76f245222e8ae1825245eda
staskosatkin <stas.kosatkin@gmail.com>
2015-04-05 12:50:39 +0300

    #1

---
+++ src/com/example/SpeechAndriod/Front.java
@@ -0,0 +1,15 @@
+package com.example.SpeechAndriod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by stas on 04.04.15.
+ */
+public class Front extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front);
+    }
+}
